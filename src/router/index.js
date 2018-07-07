5986b254e6195e02be760a5d8f879737e616ab64
goit2018 <stasyra8@gmail.com>
2018-07-07 19:39:02 +0300

    added fixes

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import MainPage from '@/components/MainPage'
-import PreviewCardBottom from '@/components/PreviewCard/PreviewCardBottom'
-
-
+import PreviewCardTop from '@/components/PreviewCard/PreviewCardTop' 
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'MainPage',
-      component: MainPage
-    },
-    {
-      path: '/PreviewCard',
-      name: 'PreviewCardBottom',
-      component: PreviewCardBottom
-    }
-  ]
+  routes: [//    { 
+    //      path: '/', 
+    //      name: 'home', 
+    //      component: home 
+    //    }, 
+    //    { 
+    //      path: '/details', 
+    //      name: 'details', 
+    //      component: details 
+    //    }, 
+    //    { 
+    //      path: '/search', 
+    //      name: 'search', 
+    //      component: search 
+    //    }, 
+      { 
+      path: '/previewcard', 
+      name: 'PreviewCardTop', 
+      component: PreviewCardTop }]
 })
